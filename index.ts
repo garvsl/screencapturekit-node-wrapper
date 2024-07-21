dc851a1087c39742fd5f96ac1ec11c9517b24c46
garvsl <garvsehgal7@gmail.com>
2024-07-20 20:39:29 -0400

    list screens

---
+++ index.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import { execFile } from "child_process";
+import { promisify } from "util";
+
+const execFileAsync = promisify(execFile);
+
+class ScreenCaptureKit {
+  private executablePath: string;
+
+  constructor() {
+    this.executablePath = path.join(__dirname, "screencapturekit", "dist");
+  }
+
+  async listScreens() {
+    try {
+      const { stdout } = await execFileAsync(this.executablePath, [
+        "list",
+        "screens",
+      ]);
+      return JSON.parse(stdout);
+    } catch (error) {
+      console.error("Error in listScreens:", error);
+      throw error;
+    }
+  }
+}
+
+export default ScreenCaptureKit;
